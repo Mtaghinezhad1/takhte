d99f13491a49ba7f9945f01de7ae457ce98e20d3
Mtaghinezhad1 <116306240+Mtaghinezhad1@users.noreply.github.com>
2026-06-08 14:41:44 +0330

    final nardland v1

--- stores/useGameStore.ts
+++ stores/useGameStore.ts
@@ -9,7 +9,7 @@ import useUserStore from './useUserStore';
 const useGameStore = create((set, get) => ({
   // State
   board: boardService.getInitialBoard(),
-  currentTurn: 'black',
+  currentTurn: 'white',
   movesCount: 0,
   allDice: [1, 1, 1, 1],
   dice: [1, 1, 1, 1],
@@ -48,7 +48,7 @@ const useGameStore = create((set, get) => ({
       aiLevel,
       aiLevelForWhite,
       aiProfile,
-      currentTurn: 'black',
+      currentTurn: 'white',
       movesCount: 0,
       dice: [1, 1],
       activeDice: 1,
